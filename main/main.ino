25df4336a10d3fcf8931972d5f276f60540bf30c
Kushagra Srivastava <kushagra1403@gmail.com>
2022-06-13 16:39:42 +0530

    sound sensor

--- main/main.ino
+++ main/main.ino
@@ -2,9 +2,12 @@
 const int trigPin = 3;
 const int echoPin = 7;
 const float soundSpeed = 0.034; //unit: cm/us
-const float avgWalkingSpeed = 1.3; //unit: m/s
+const float avgWalkingSpeed = 130; //unit: cm/s
+const float timeToWait = 100; //unit: ms
+const float lightChangeWindow = 2000; //unit: ms
 long duration;
 int distance;
+int distance1 = 0, distance2 = 0;
 int currentState = 0; //0: neutral, 1: enter, 2: exit
 int GetDistance();
 int GetCurrentState();
@@ -14,6 +17,7 @@ const int rPin = 9;
 const int gPin = 10;
 const int bPin = 11;
 int lightType = 0; //0-off, 1-white, 2-r, 3-g, 4-b
+bool lightChanged = false;
 void LightDueToWalk(int);
 void LightDueToClap(int);
 void RGBLight(int, int, int);
@@ -31,6 +35,12 @@ long currentClapTime = 0;
 long lastLightChange = 0;
 int ClapType();
 
+//Relay
+const int fan = 3;
+const int bulb = 5;
+const int charger = 4;
+const int bell = 6;
+
 void setup()
 {
     pinMode(trigPin, OUTPUT); 
@@ -42,6 +52,11 @@ void setup()
 
     pinMode(soundPin, INPUT);
 
+    pinMode(fan, OUTPUT);
+    pinMode(bulb, OUTPUT);
+    pinMode(charger, OUTPUT);
+    pinMode(bell, OUTPUT);
+
     Serial.begin(9600);
 }
 
@@ -50,10 +65,12 @@ void loop()
     distance = GetDistance();
 
     currentState = GetCurrentState();   
-
+    
     LightDueToWalk(currentState);
     LightDueToClap(ClapType());
     AssignLight(lightType);
+
+    Serial.println(sound);
 }
 
 int GetDistance()
@@ -70,23 +87,24 @@ int GetDistance()
 
 int GetCurrentState()
 {
-    int curState = -1;
+    int curState;
 
-    int timeToWait = 250; //unit: ms
-    int distance1 = 0, distance2 = 0;
-
-    distance1 = distance;
+    distance1 = GetDistance();
     delay(timeToWait);
-    distance2 = distance;
+    distance2 = GetDistance();;
 
-    float speed = ((distance2 - distance1)/100) / (timeToWait/1000); //unit: m/s
+    double speed = (distance1 - distance2) / (timeToWait/1000); //unit: cm/s
 
     if (speed < -avgWalkingSpeed)
-        curState = 1;
-    else if (speed > avgWalkingSpeed)
+    {
         curState = 2;
+    }
+    else if (speed > avgWalkingSpeed)
+    {
+        curState = 1;
+    }
     else
-        curState = 0;
+       curState = 0;
 
     return curState;
 }
@@ -95,9 +113,9 @@ void LightDueToWalk(int curState)
 {
     if (curState == 1)
     {
-        lightType = 1;
+        lightType = 1;       
     }
-    else
+    else if (curState == 2)
     {
         lightType = 0;
     }
@@ -129,19 +147,19 @@ void LightDueToWalk(int curState)
 int ClapType()
 {
     int claps = 0;
-    sound = digitalRead(soundPin);
+    sound = analogRead(soundPin);
     currentClapTime = millis();
 
-    if (sound == 1) 
+    if (sound > 600) 
     { 
         if ((currentClapTime > lastClapTime + clapWindowMin) && (lastSound == 0) && (currentClapTime < lastClapTime + clapWindowMax) && (currentClapTime > lastLightChange + timeout)) 
         { 
             claps = 2;
-            lastLightChange = currentNoiseTime;
+            lastLightChange = currentClapTime;
         }
         else
             claps = 1;        
-        lastNoiseTime = currentNoiseTime;
+        lastClapTime = currentClapTime;
     }
     lastSound = sound;
     
@@ -152,7 +170,7 @@ void LightDueToClap(int claps)
 {
     if (claps == 1)
     {
-        lightType = (lightType == 0) ? 1 ; 0;  
+        lightType = (lightType == 0) ? 1 : 0;  
     }
     else if (claps == 2)
     {
@@ -175,7 +193,7 @@ void AssignLight(int lightType)
             RGBLight(0, 0, 0);
             break;  
         case(1):
-            RGBLight(255, 255, 255);
+            RGBLight(200, 255, 255);
             break;  
         case(2):
             RGBLight(255, 0, 0);
@@ -193,5 +211,67 @@ void RGBLight(int r, int g, int b)
 {
     analogWrite(rPin, r);
     analogWrite(gPin, g);
-    analogWrite(bPin, b);    
+    analogWrite(bPin, b);  
+    //delay(lightChangeWindow);  
+}
+
+void Relay()
+{
+    if(Serial.available() == 1)
+    {
+        String val = Serial.readString();
+        //Serial.println(val);
+        //For Fan
+        if (val == "fan on")
+        {
+            digitalWrite(fan, HIGH);
+        }
+        else if (val == "fan off")
+        {
+            digitalWrite(fan, LOW);
+        }
+        //For bulb
+        if (val == "bulb on")
+        {
+            digitalWrite(bulb, HIGH);
+        }
+        else if (val == "bulb off")
+        {
+            digitalWrite(bulb, LOW);
+        }
+        //For charger
+        if (val == "charger on")
+        {
+            digitalWrite(charger, HIGH);
+        }
+        else if (val == "charger off")
+        {
+            digitalWrite(charger, LOW);
+        }
+        //For bell
+        if (val == "bell on")
+        {
+            digitalWrite(bell, HIGH);
+        }
+        else if (val == "bell off")
+        {
+            digitalWrite(bell, LOW);
+        }
+        //for everything
+        
+        if (val == "all on")
+        {
+            digitalWrite(fan, HIGH);
+            digitalWrite(bulb, HIGH);
+            digitalWrite(charger, HIGH);
+            digitalWrite(bell, HIGH);
+        }
+        else if (val == "all off")
+        {
+            digitalWrite(bulb, LOW);
+            digitalWrite(fan, LOW);
+            digitalWrite(charger, LOW);
+            digitalWrite(bell, LOW);
+        }
+    }
 }
